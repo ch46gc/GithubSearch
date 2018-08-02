be4621318b03912d366f8dc984f9e650b51ea6ff
ch46gc <chriseyhillgen@gmail.com>
2018-08-02 19:52:45 -0400

    add AsyncTaskLoader

--- app/src/main/java/com/example/android/githubsearch/MainActivity.java
+++ app/src/main/java/com/example/android/githubsearch/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.android.githubsearch;
 
+
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,84 +20,125 @@ import com.example.android.githubsearch.utilites.NetworkUtils;
 import java.io.IOException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    /*Saves and restore the URL that is being displayed*/
+    private static final String SEARCH_QUERY_URL_EXTRA = "query";
+    /*Saves and restores the JSON that is being displayed*/
+    private static final String SEARCH_RESULTS_RAW_JSON = "results";
+
     private EditText mSearchBoxEditText;
     private TextView mUrlDisplayTextView;
     private TextView mSearchResultsTextView;
     private TextView mErrorMessageDisplay;
     private ProgressBar mLoadingIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSearchBoxEditText = findViewById(R.id.et_search_box);
         mUrlDisplayTextView = findViewById(R.id.tv_url_display);
-        mSearchResultsTextView =findViewById(R.id.tv_github_search_results_json);
-        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
-        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
+        mErrorMessageDisplay =  findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+        if (savedInstanceState != null) {
+            String queryUrl = savedInstanceState.getString(SEARCH_QUERY_URL_EXTRA);
+            String rawJsonSearchResults = savedInstanceState.getString(SEARCH_RESULTS_RAW_JSON);
+            mUrlDisplayTextView.setText(queryUrl);
+            mSearchResultsTextView.setText(rawJsonSearchResults);
         }
+    }
 
-
-        private void makeGithubSearchQuery() {
+    private void makeGithubSearchQuery() {
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
         new GithubQueryTask().execute(githubSearchUrl);
-        }
-        private void showJsonDataView(){
+    }
+
+    private void showJsonDataView() {
         mErrorMessageDisplay.setVisibility(View.INVISIBLE);
         mSearchResultsTextView.setVisibility(View.VISIBLE);
-        }
+    }
 
-        private void showErrorMessage(){
+    private void showErrorMessage() {
         mSearchResultsTextView.setVisibility(View.INVISIBLE);
         mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        public void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
         }
-        public class GithubQueryTask extends AsyncTask<URL, Void, String> {
-            @Override
-            public void onPreExecute() {
-                super.onPreExecute();
-                mLoadingIndicator.setVisibility(View.VISIBLE);
-            }
-            @Override
-            protected String doInBackground(URL... urls) {
-                URL searchUrl = urls[0];
-                String githubSearchResults = null;
-                try {
-                    githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return githubSearchResults;
-            }
-
-            @Override
-            protected void onPostExecute(String githubSearchResults) {
-                mLoadingIndicator.setVisibility(View.INVISIBLE);
-                if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                    showJsonDataView();
-                    mSearchResultsTextView.setText(githubSearchResults);
-                } else {
-                    showErrorMessage();
-                }
-            }
-
-            }
 
         @Override
-        public boolean onCreateOptionsMenu(Menu menu){
-            getMenuInflater().inflate(R.menu.main, menu);
+        protected String doInBackground(URL... param) {
+            URL searchUrl = param[0];
+            String githubSearchResults = null;
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String githubSearchResults) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (githubSearchResults != null && !githubSearchResults.equals("")) {
+                showJsonDataView();
+                mSearchResultsTextView.setText(githubSearchResults);
+            } else {
+                showErrorMessage();
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if (menuItemThatWasSelected == R.id.action_search) {
+            makeGithubSearchQuery();
             return true;
         }
+        return super.onOptionsItemSelected(item);
+    }
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item){
-            int menuItemThatWasSelected = item.getItemId();
-            if(menuItemThatWasSelected == R.id.action_search){
-                makeGithubSearchQuery();
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String queryUrl = mUrlDisplayTextView.getText().toString();
+        outState.putString(SEARCH_QUERY_URL_EXTRA, queryUrl);
+            String rawJsonSearchResults = mSearchResultsTextView.getText().toString();
+            outState.putString(SEARCH_RESULTS_RAW_JSON, rawJsonSearchResults);
         }
     }
 
